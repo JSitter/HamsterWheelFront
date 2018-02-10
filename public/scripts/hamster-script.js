925b2b66bcd319ac3c6bbdaaf7f7ee541fab85e8
Justin Sitter <justin@jaytria.com>
2018-02-10 14:09:14 -0800

    changed colors

--- public/scripts/hamster-script.js
+++ public/scripts/hamster-script.js
@@ -1,5 +1,10 @@
 var navigation = $(".navigation");
 
+$(document).ready(function(){
+
+})
+
+
 $(window).scroll(function(){
     if( $(this).scrollTop() > 400) {
         navigation.addClass("main-nav-scrolled");
